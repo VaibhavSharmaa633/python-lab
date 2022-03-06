5b4567b299bf6f9ec2203c7f23433a70fa948344
VaibhavSharmaa633 <100210610+VaibhavSharmaa633@users.noreply.github.com>
2022-03-06 17:29:43 +0530

    Add files via upload

---
+++ vowel and consonent.py
@@ -0,0 +1,8 @@
+#vowel and consonent
+x=input("Enter a string:")
+
+if(x=='a' or x=='e' or x=='i' or x=='o' or x=='u' or x=='A' or x=='E' or x=='I' or x=='O' or x=='U'):
+    print("It is a vowel character")
+else:
+    print("It is a consonent")
+    
