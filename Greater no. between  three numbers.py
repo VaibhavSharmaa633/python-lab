5e799b84b74bb04f078597ffb29f063e1b01d320
VaibhavSharmaa633 <100210610+VaibhavSharmaa633@users.noreply.github.com>
2022-03-06 15:05:40 +0530

    Add files via upload

---
+++ Greater no. between  three numbers.py
@@ -0,0 +1,10 @@
+#Greater no. b/w three numbers
+x=int(input("Enter the 1st no."))
+y=int(input("Enter the 2nd no."))
+z=int(input("Enter the 3rd no."))
+if(x>y):
+    print("Greater no. is:",x)
+elif(y>z):
+    print("Greaste no. is:",y)
+else:
+    print("Greater no. is:",z)
